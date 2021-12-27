a1ad9dfdd919f2aac8b9bafa33ce0b676c57be6b
neumanf <fabricionewman@protonmail.com>
2021-12-27 10:34:35 +0000

    feat: add weapon model

---
+++ HuntShowdownAPI/Models/Weapon.cs
@@ -0,0 +1,32 @@
+namespace HuntShowdownAPI.Models;
+
+public class Weapon
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Cost { get; set; }
+    
+    public string UnlockCondition { get; set; }
+    
+    public string Ammunition { get; set; }
+    
+    public string Capacity { get; set; }
+    
+    public int Damage { get; set; }
+    
+    public string RateOfFire { get; set; }
+    
+    public string ReloadSpeed { get; set; }
+    
+    public int MeleeDamage { get; set; }
+    
+    public string EffectiveRange { get; set; }
+    
+    public string Handling { get; set; }
+    
+    public string MuzzleVelocity { get; set; }
+    
+    public int HeavyMeleeDamage { get; set; }
+}
